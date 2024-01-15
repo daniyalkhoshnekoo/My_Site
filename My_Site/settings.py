4a896e720123220a2bd3d9daf6fdc451a2b7c560
daniyal <daniyalkhoshnekoo77@outlook.com>
2024-01-15 17:10:08 +0330

    static files

--- My_Site/settings.py
+++ My_Site/settings.py
@@ -118,14 +118,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/statics/'
 STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'statics',
+    BASE_DIR / "statics",
 ]
 
 # Default primary key field type
--- website/views.py
+++ website/views.py
@@ -3,11 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def home_view(request):
-    return HttpResponse('hello world')
+def index_view(request):
+    return render (request, 'website/index.html')
 
 def contact_view(request):
-    return HttpResponse('<h1>Contact page</h1>')
+    return render (request, 'website/contact.html')
 
 def about_view(request):
-    return HttpResponse('<h1>About page</h1>')
+    return render (request, 'website/about.html')
