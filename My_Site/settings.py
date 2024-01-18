663b9be014f23a5f1e94d6e13a77273eabc02bbb
daniyal <daniyalkhoshnekoo77@outlook.com>
2024-01-18 17:01:16 +0330

    blog app

--- My_Site/settings.py
+++ My_Site/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website.app.WebsiteConfig',
+    'website.apps.WebsiteConfig',
+    'blog'
 ]
 
 MIDDLEWARE = [
